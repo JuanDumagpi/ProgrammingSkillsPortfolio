7fa4bc119dde43da782c5e87563c230a91e40fb0
JuanDumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-09-12 00:14:17 -0700

    Delete Chapter 1- Getting Started with Python/Exercise1.py

--- Chapter 1- Getting Started with Python/Exercise1.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-print Poem
-"""
-print("""Twinkle, twinkle, little star, How I wonder what you are! \n\t\t Up above the world so high, \n\t\t Like a diamond in the sky. \n\t\t\t Twinkle, twinkle, little star,\nHow I wonder what you are""")
-
