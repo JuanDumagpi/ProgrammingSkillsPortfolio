6875b44f3a4f5596f5a83f2502af2425174542d7
JuanDumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-09-19 01:01:02 +0400

    Add files via upload

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Guest List
+"""
+guest = ["Elon Musk", "God", "Vincent Van Gogh"]
+
+print("Greetings,", guest[0], "I would like to cordially invite you to a 'dinner slash making fun of you for being an absolute idiot' night.")
+print("Hello!", guest[1], "you are invited to share a night of dining and drinking with a very curious atheist.")
+print("Mr.", guest[2], ", you are hereby invited to a celebration of your artistry and lifetime work over dinner.")
