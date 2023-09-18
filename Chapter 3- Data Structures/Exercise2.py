88f4d09080a94a0778db469de2100c4edfc5ff0e
JuanDumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-09-19 00:58:31 +0400

    Add files via upload

---
+++ Chapter 3- Data Structures/Exercise2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Greetings
+"""
+
+namelist = ["John", "Mars", "Sid", "Ron", "Jan"]
+print("Hello There, General" ,namelist[0] ,"you are a bold one!")
+print("Hello There, General" ,namelist[1] ,"you are a bold one!")
+print("Hello There, General" ,namelist[2] ,"you are a bold one!")
+print("Hello There, General" ,namelist[3] ,"you are a bold one!")
+print("Hello There, General" ,namelist[4] ,"you are a bold one!")
---
+++ Chapter 3- Data Structures/Exercise3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Your Own List
+"""
+transpo = ["Yamaha Vino", "Brough Superior SS100", "1967 Chevrolet Impala"]
+print("I would love to have a" ,transpo[1] ,"to ride around and explore the world.")
+print("Revving up a" ,transpo[2] ,"produces a really wonderful sound.")
+print("Going camping with a" ,transpo[0] ,"while taking in the sights would be so relaxing.")
