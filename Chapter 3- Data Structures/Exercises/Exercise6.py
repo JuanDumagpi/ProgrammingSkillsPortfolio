43542e78134ca2b23805453e1986d4df758cb91c
JuanDumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-09-19 11:15:29 +0400

    Add files via upload

--- Chapter 3- Data Structures/Exercises/Exercise6.py
+++ Chapter 3- Data Structures/Exercises/Exercise6.py
@@ -1,18 +1,31 @@
-# -*- coding: utf-8 -*-
 """
-IF CONDITION
+Shrinking Guest List
 """
 
-#AGE INPUT
-age=int(input("Please enter your age "))
-if age<=4:
-    print("You're too young mate, get your parents.")
-else:
-    print("Welcome, kid.")
-    
+#Guest List
+guest = ["Carl Sagan", "God", "Vincent Van Gogh"]
+print("It has come to my attention that my new dinner table will not arrive today. As such there will be a change in guests.")
+
+#space to make it look clean
+print("\n") 
+
+#removing guest, and showing updated list
+print("Removed guests:", guest.pop(1))
+print("Updated guest list:", guest)
+
 print("\n")
-a=int(input("Enter a number to check for even or odd "))
-if (a%2==0):
-    print(a, "is an even number")
-else:
-    print(a, "is an odd number")
+
+#showing guests with unchanged status
+print(guest[0], "your status as a guest has not changed and we welcome you to dinner.")
+print(guest[1], "your status as a guest has not changed and we welcome you to dinner.")
+
+#deleting guest from list
+del guest[1]
+del guest[0]
+#printing empty list, and showing that the list is empty
+print(guest)
+
+print("\n")
+
+if guest==[]:
+    print("You have no people left in the guest list.")
