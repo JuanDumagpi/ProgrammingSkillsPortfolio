19e41873c89c009dcebac37c2038073afbe5f1e8
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:13:38 +0400

    Update Exercise2.py

--- Chapter 6- Loops/Exercise2.py
+++ Chapter 6- Loops/Exercise2.py
@@ -6,14 +6,14 @@ Exercise 2 - Movie Tickets
 #creates an infinite loop
 while True:
     
-    #input for age
+    #lets the user type in their age
     x = int(input("Please enter your age:"))
 
-    #checks the input and prints the appropriate message
-    if x<3:
+    #checks the age input and prints the appropriate message
+    if x<3: #if the age is less than 3, prints the message below
         print ("Under 3, ticket's free!")
-    elif x>=3 and x<=12:
+    elif x>=3 and x<=12: #if the age is equal to three but less than or equal to 12, prints the message below
         print ("Ticket is $10")
-    else:
+    else: #if it's not any of the above, prints the message below
         print ("Ticket is $15")
     
