073a280d1624ac4622e38bb7bd690dc456d7fefd
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 18:24:16 +0400

    Update Exercise1 - Pass.py

--- Chapter 4- Control Flow/Exercises/Exercise1 - Pass.py
+++ Chapter 4- Control Flow/Exercises/Exercise1 - Pass.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Alien Colors #1
-fail
+pass
 """
 
 #sets alien color
-alien_color = "blue"
+alien_color = "green"
 
 #this tests if the alien color is green and if true, displays a message
 if alien_color == "green":
-    print ("You obtain 5 points")
+    print ("You obtain 5 points") #now that the right alien blood color is checked, it will print this message
