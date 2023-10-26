6dfd7b06fbd8d31caae8f5721fd655dcdfd18b98
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:23:37 +0400

    Update Exercise1.py

--- Chapter 7- Functions/Exercises/Exercise1.py
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -3,10 +3,11 @@
 Chapter 7 - Functions
 Ex 1 - Messages
 """
-
+#defines a function, this lets us create code that will only run once we call its function
 def display_message():
     print("I'm learning functions!")
     
+#here, I'm calling the functions three times, separated by a line space
 display_message()
 print("\n")
 display_message()
