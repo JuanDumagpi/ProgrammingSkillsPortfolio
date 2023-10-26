d7d4d47b4b177d9b4dffbb4c1e88941d4db69c3d
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 14:03:18 +0400

    Update Exercise1.py
    Added comments.

--- Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -3,5 +3,6 @@
 Exercise 1: Print Strings
 """
 #String printed with formatting, \n for new line, and \t for tab. This shows the message as a cascading paragraph
+# \n makes a line break while \t makes a tab space
 print("""Twinkle, twinkle, little star, How I wonder what you are! \n\t\t Up above the world so high, \n\t\t Like a diamond in the sky. \n\t\t\t Twinkle, twinkle, little star,\nHow I wonder what you are!""")
 
