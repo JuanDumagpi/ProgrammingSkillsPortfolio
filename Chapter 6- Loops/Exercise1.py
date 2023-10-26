89ebeed2e198dee1a94b643644aa1c35767336d8
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:05:44 +0400

    Update Exercise1.py

--- Chapter 6- Loops/Exercise1.py
+++ Chapter 6- Loops/Exercise1.py
@@ -9,12 +9,12 @@ ptop = []
 #creates an infinite loop until broken
 while True:
     
-    #input for pizza toppings
+    #lets the user add toppings by typing down what they want
     x = input("Enter what you want to put on your pizza or type 'Done' to finish:")
-    #checks if the input is done and breaks the loop
+    #this checks if they user has typed "Done", if true, then breaks the loop
     if x == "Done":
         break
-    #adds the input to the list if not done
+    #adds the user's input to the topping list and displays a message, [-1] being the most recent item in the list 
     else:
         ptop.append(x)
         print(ptop[-1], "has been added to your pizza.")
