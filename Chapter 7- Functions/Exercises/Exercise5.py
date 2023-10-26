04761780bf8f2c836413f523ffee8b7d93d97d2f
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:42:56 +0400

    Update Exercise5.py

--- Chapter 7- Functions/Exercises/Exercise5.py
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -3,11 +3,13 @@
 Chapter 7 - Functions
 Ex 5 - Cities
 """
+#this defines a function to print a statement with default arguments
 def describe_city(name="New York", country="USA"):
     print (name, "is in", country)
 
+#calls the describe_city function which will print the message above, since there are no values for arguments, it will default
 describe_city()
 
+#these will will call the describe_city function but changes the arguments, so it will not default
 describe_city("Manila", "Philippines")
-
-describe_city("Paris", "Paris")
+describe_city("Paris", "France")
