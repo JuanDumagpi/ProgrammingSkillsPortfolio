75df526c6ee5b331638bb00a86f4583aa51b3cd3
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:38:58 +0400

    Update Exercise4.py

--- Chapter 7- Functions/Exercises/Exercise4.py
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -3,12 +3,15 @@
 Chapter 7 - Functions
 Ex 5 - TShirt
 """
-#Defualt arguments added
+#Default arguments added, which means, there is going to be a set value for the argument if no new ones are specified
 def make_shirt(size = "Large", message="I love Python"):
     print ("Size:", size, "\n", "Message:", message)
 
+#Here, it prints a message that changes the (size), but not the message which will then use the default 
 make_shirt(size = "Medium")
 print("\n")
+#Here, it prints a message. Since there are no changes to the argument, will use the default
 make_shirt()
 print("\n")
+#this changes both size and message, so it will not use the default
 make_shirt(message="Is Justice.", size="Small")
