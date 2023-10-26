57de16c4a4fba342e9b09dca130e6fe807435e6a
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:26:17 +0400

    Update Exercise2.py

--- Chapter 7- Functions/Exercises/Exercise2.py
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -3,10 +3,11 @@
 Chapter 7 - Functions
 Ex 2 - Fave Book
 """
-
+#In this function, there is an argument for (title), so when the function is called, it lets us set the argument
 def Favorite_Book(title):
     print("My favorite book is ", title)
 
+#Here we are calling the function with a specific argument, this will print out message in the function and use the argument to replace (title)
 Favorite_Book("The Hobbit")
 Favorite_Book("The Hitchhiker's Guide to the Galaxy")
 Favorite_Book("House of Leaves")
