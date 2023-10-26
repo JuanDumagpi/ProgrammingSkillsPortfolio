cada458bfa4da421c36733b933fd32189caf8545
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:18:37 +0400

    Update Exercise5.py

--- Chapter 6- Loops/Exercise5.py
+++ Chapter 6- Loops/Exercise5.py
@@ -6,10 +6,12 @@ Exercise4 - Deli
 #list of orders
 sandwhich_orders=["PB&J", "Grilled Cheese", "Tuna","Pastrami","Pastrami", "Pastrami"]
 
+#a message that says Pastrami is sold out
 print("ATTENTION: Pastrami is 86")
 
-#This makes a loop that looks for all Pastrami  then removes Pastrami
+#This makes a loop that looks for all Pastrami in the sandwhich_orders list then removes all the Pastrami
 while "Pastrami" in sandwhich_orders:
     sandwhich_orders.remove("Pastrami")
-    
+
+#prints a message that shows the updated list
 print (sandwhich_orders)
