bfc925259db705d1d53c32858c8bfa5cb4d3964d
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 18:19:18 +0400

    Update Exercise1.py

--- Chapter 3- Data Structures/Exercises/Exercise1.py
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -5,7 +5,7 @@ Exercise 1: Names
 #Variable for the list of names
 namelist = ["John", "Mars", "Sid", "Ron", "Jan"]
 
-#Accessing each person in the list by printing each name in no specific order
+#Accessing each person in the list by printing each name in no specific order. The number denotes the position of the name with 0 being the first one.
 print(namelist[0])
 print(namelist[2])
 print(namelist[3])
