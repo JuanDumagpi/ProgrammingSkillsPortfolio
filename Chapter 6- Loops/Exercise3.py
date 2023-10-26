4efac993d4a3ef98b2cbc03b4d85707d5b560c2c
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 19:16:18 +0400

    Update Exercise3.py

--- Chapter 6- Loops/Exercise3.py
+++ Chapter 6- Loops/Exercise3.py
@@ -3,8 +3,8 @@
 Chapter 6 Loops
 Exercise 3 - Infinity
 """
-
+#checks if the statement is true, which, since it will not be changed in this code will not change, making an infinite loop.
 while True:
-    
-    print("Dormammu I've come to bargain")
+    #this message will continue to print endlessly
+    print("Dormammu, I've come to bargain.")
     
