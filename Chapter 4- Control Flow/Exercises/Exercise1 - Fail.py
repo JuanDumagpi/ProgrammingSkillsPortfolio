39052bb756bbc725fa5e96a3ad5659b584fe6ae3
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-10-26 18:23:21 +0400

    Update Exercise1 - Fail.py

--- Chapter 4- Control Flow/Exercises/Exercise1 - Fail.py
+++ Chapter 4- Control Flow/Exercises/Exercise1 - Fail.py
@@ -8,5 +8,5 @@ fail
 alien_color = "blue"
 
 #this tests if the alien color is green and if true, displays a message
-if alien_color == "green": #so, if the color of the alien's blood is green
-    print ("You obtain 5 points") #it will print this message
+if alien_color == "green": #this says, if the alien's blood is green, print the below message
+    print ("You obtain 5 points") #since the alien's blood is blue, it won't print
