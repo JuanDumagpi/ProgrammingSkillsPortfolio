ca5b0c47b063464c5e139e33fa3f0623385c2b65
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2023-12-28 14:25:01 +0400

    Add files via upload

--- VMachine.py
+++ VMachine.py
@@ -2,25 +2,26 @@
 """
 Vending Machiiiiine!
 """
-
+#//// Dictionaries/Variables ////
 vm = {"C1":"Coke", "C2":"Sprite", "C3":"Fanta", "H1":"Coffee", "H2":"Tea", "H3":"Hot Chocolate", "S1":"Kitkat", "S2":"Chips", "S3":"Snickers"} #Dictionary for the vending machine stock with Code:Item Name
-prices = {"Coke":1, "Sprite":1, "Fanta":1, "Coffee":2, "Tea":3, "Hot Chocolate":2, "Kitkat":1, "Chips":2, "Snickers":2} #Dictionary for prices with Item Name:Price
-s = {"Coke":25, "Sprite":25, "Fanta":20, "Coffee":15, "Tea":20, "Hot Chocolate":10, "Kitkat":10, "Chips":2, "Snickers":8}#Dictionary for the vending machine's stock, with Item Name:Stock
+prices = {"Coke":0.5, "Sprite":0.5, "Fanta":0.5, "Coffee":2, "Tea":3, "Hot Chocolate":2, "Kitkat":1, "Cheetos":2.5, "Snickers":2} #Dictionary for prices with Item Name:Price
+s = {"Coke":25, "Sprite":25, "Fanta":20, "Coffee":15, "Tea":20, "Hot Chocolate":10, "Kitkat":10, "Cheetos":2, "Snickers":8}#Dictionary for the vending machine's stock, with Item Name:Stock
 money = 0
+#//// Dictionaries/Variables ////
 
 #/////Inventory Function///
 #This will show the current stock
 def inventory():
     #Dictionary for the inventory, along with the info in string format
-    inventory={ "C1":{"Price":"$1","Name":"Coke"},
-                "C2":{"Price":"$1","Name":"Sprite"},
-                "C3":{"Price":"$1","Name":"Fanta"},
-                "H1":{"Price":"$2","Name":"Coffee"},
-                "H2":{"Price":"$3","Name":"Tea"},
-                "H3":{"Price":"$2","Name":"Hot Chocolate"},
-                "S1":{"Price":"$1","Name":"KitKat"},
-                "S2":{"Price":"$2","Name":"Chips"},
-                "S3":{"Price":"$2","Name":"Snickers"},
+    inventory={ "C1":{"Price":"$0.5","Name":"Coke"},
+                "C2":{"Price":"$0.5","Name":"Sprite"},
+                "C3":{"Price":"$0.5","Name":"Fanta"},
+                "H1":{"Price":"$2  ","Name":"Coffee"},
+                "H2":{"Price":"$3  ","Name":"Tea"},
+                "H3":{"Price":"$2  ","Name":"Hot Chocolate"},
+                "S1":{"Price":"$1  ","Name":"KitKat"},
+                "S2":{"Price":"$2.5","Name":"Cheetos"},
+                "S3":{"Price":"$2  ","Name":"Snickers"},
                 }
     print ("~~~~Here's what's available:~~~~")
     for code, info in inventory.items():
@@ -58,12 +59,12 @@ def buy():
     buyloop = True
     inventory() #calls the inventory function which shows the items available for purchase
     while buyloop == True: #buy function loop
-        item = input("Item Code here:")  #lets you input the item code
+        item = input("Please input the item code here:")  #lets you input the item code
         if item in vm: #input code is checked if it's in the vending machine's list of items
             itemname = vm[item]
             if s[itemname] > 0: #checks if the item has stock
                 if money - prices[itemname] >= 0: #checks if you can afford the item
-                    print ("Dispensing", vm[item]) #dispenses the item after the above checks are true
+                    print ("~~~~~~~~~~Dispensing~~~~~~~~~~~~", "\n~~~~~ Enjoy your",vm[item],"~~~~~~") #dispenses the item after the above checks are true
                     money -= prices[itemname] #deducts money based on the item's price
                     print("Available funds: $", money) #shows the available remaining funds
                     s[itemname] += -1 #deducts stock
@@ -73,13 +74,13 @@ def buy():
                     else:
                         buyloop = False
                 else:
-                    print("Insufficient funds, please add more, and try again.")#error if there is less funds than the price
+                    print("//// Insufficient funds, please add more, and try again. ////")#error if there is less funds than the price
                     buyloop = False
             else:
-                print("Out of stock, please try again.")#error if there is no remaining stock for the code given
+                print("//// Out of stock, please try again. ////")#error if there is no remaining stock for the code given
                 buyloop = False
         else:
-                print("Invalid code, please try again.")#error if user inputs an invalid code
+                print("//// Invalid code, please try again. ////")#error if user inputs an invalid code
                 buyloop = False
 #/////Buy Function/////
 
@@ -89,6 +90,7 @@ def mainmenu():
     choice = 0
     global money
     while menuloop == True: #creates a loop for the main menu
+        print ("~~~~~ Welcome to my Vending Machine! ~~~~~")
         print ("1 Insert Credits - Currently: $",money, sep="")
         print ("2 Purchase Items")
         print ("3 Done")
